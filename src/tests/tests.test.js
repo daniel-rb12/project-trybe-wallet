0231329712e56b8827464418aa00c683912785b9
Daniel Ribeiro <daniel.ribeiro012@hotmail.com>
2022-12-10 11:52:49 -0300

    Requisito 5

---
+++ src/tests/tests.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import { renderWithRouterAndRedux } from './helpers/renderWith';
+import userEvent from '@testing-library/user-event';
+
+it('A página deve conter um campo de email', () => {
+  renderWithRouterAndRedux(<App />);
+  const inputEmail = screen.getByRole('textbox', { name: 'E-mail' });
+  expect(inputEmail).toBeInTheDocument();
+});
+
+it('A página deve conter um campo de senha', () => {
+  renderWithRouterAndRedux(<App />);
+  const inputPassword = screen.getByLabelText(/senha/i);
+  expect(inputPassword).toBeInTheDocument();
+});
+
+it('A página deve conter um botão de entrar', () => {
+  renderWithRouterAndRedux(<App />);
+  const buttonLogin = screen.getByRole('button', { name: /entrar/i });
+  expect(buttonLogin).toBeInTheDocument();
+});
+it('É possível logar com email e senha e ser redirecionado', async () => {
+  renderWithRouterAndRedux(<App />);
+  const inputEmail = screen.getByRole('textbox', { name: 'E-mail' });
+  const inputPassword = screen.getByLabelText(/senha/i);
+  const buttonLogin = screen.getByRole('button', { name: /entrar/i });
+
+  userEvent.type(inputEmail, 'teste@teste.com');
+  userEvent.type(inputPassword, '123456');
+  userEvent.click(buttonLogin);
+
+  const valueLabel = screen.getByText(/valor:/i);
+
+  expect(valueLabel).toBeInTheDocument();
+});
