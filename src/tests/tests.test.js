8d60303bba12b84af71af18ea42876832e0393a5
Daniel Ribeiro <daniel.ribeiro012@hotmail.com>
2022-12-12 11:49:00 -0300

    Requisito 6

--- src/tests/tests.test.js
+++ src/tests/tests.test.js
@@ -1,28 +1,35 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
 import App from '../App';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
-import userEvent from '@testing-library/user-event';
 
 it('A página deve conter um campo de email', () => {
   renderWithRouterAndRedux(<App />);
+
   const inputEmail = screen.getByRole('textbox', { name: 'E-mail' });
+
   expect(inputEmail).toBeInTheDocument();
 });
 
 it('A página deve conter um campo de senha', () => {
   renderWithRouterAndRedux(<App />);
+
   const inputPassword = screen.getByLabelText(/senha/i);
+
   expect(inputPassword).toBeInTheDocument();
 });
 
 it('A página deve conter um botão de entrar', () => {
   renderWithRouterAndRedux(<App />);
+
   const buttonLogin = screen.getByRole('button', { name: /entrar/i });
+
   expect(buttonLogin).toBeInTheDocument();
 });
 it('É possível logar com email e senha e ser redirecionado', async () => {
   renderWithRouterAndRedux(<App />);
+
   const inputEmail = screen.getByRole('textbox', { name: 'E-mail' });
   const inputPassword = screen.getByLabelText(/senha/i);
   const buttonLogin = screen.getByRole('button', { name: /entrar/i });
