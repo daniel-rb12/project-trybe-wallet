850d2e960e315f13fa8c7e1acd4a0dbfb8eae83e
Daniel Ribeiro <daniel.ribeiro012@hotmail.com>
2022-12-27 16:40:37 -0300

    Instalação tailwind + estilização da página de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getEmail } from '../redux/actions';
+import '../css/login.css';
 
 const PASSWORD_LENGTH = 6;
 
@@ -40,37 +41,50 @@ class Login extends React.Component {
   render() {
     const { email, password, buttonDisable } = this.state;
     return (
-      <form>
-        <label htmlFor="email">
-          E-mail
-          <input
-            data-testid="email-input"
-            type="email"
-            name="email"
-            id="email"
-            value={ email }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="password">
-          Senha
-          <input
-            data-testid="password-input"
-            type="password"
-            name="password"
-            id="password"
-            value={ password }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <button
-          type="button"
-          disabled={ buttonDisable }
-          onClick={ this.buttonAction }
-        >
-          Entrar
-        </button>
-      </form>
+      <div className="flex justify-center items-center h-screen">
+        <form className="max-w-sm m-4 p-10 bg-black bg-opacity-80 rounded shadow-xl">
+          <p className="text-white font-medium text-center text-lg font-bold">LOGIN</p>
+          <label className="block pt-2 pb-5 text-sm text-white" htmlFor="email">
+            E-mail
+            <input
+              data-testid="email-input"
+              className="w-full mt-1 px-5 py-1 text-gray-700 bg-gray-600
+              rounded focus:outline-none focus:bg-white"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Digite seu e-mail"
+              value={ email }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label className="block text-sm text-white" htmlFor="password">
+            Senha
+            <input
+              data-testid="password-input"
+              className="w-full px-5 mt-1 mb-6 py-1 text-gray-700 bg-gray-600 rounded
+              focus:outline-none focus:bg-white"
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Digite sua senha"
+              value={ password }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <div className="flex justify-center w-full">
+            <button
+              type="button"
+              className="px-4 py-1 text-white font-light bg-sky-500
+              disabled:bg-gray-300 rounded"
+              disabled={ buttonDisable }
+              onClick={ this.buttonAction }
+            >
+              Entrar
+            </button>
+          </div>
+        </form>
+      </div>
     );
   }
 }
